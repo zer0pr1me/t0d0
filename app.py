5f8a0f636eb7f8065a9f740e99957cc41ee583f6
0 <zeroprime@mailbox.org>
2025-12-09 03:11:10 +0200

    implement selection and saving/loading

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
+import json
+from dataclasses import dataclass, asdict
+
 from textual.app import App, ComposeResult
 from textual import events
 from textual.widgets import Checkbox
 
+@dataclass
+class Todo:
+    text: str
+    done: bool
+
 class MyApp(App):
+    filename = 'todo-list.json'
+    selected_index = 0
+
     def compose(self) -> ComposeResult:
-        yield Checkbox('Add test TODO items', True)
-        yield Checkbox('Implement focusing items', False)
-        yield Checkbox('Implement checking unchecking items', False)
+        with open(self.filename, 'r') as f:
+            self.todos = [Todo(**todo) 
+                          for todo in json.load(f)]
+
+        for i, todo in enumerate(self.todos):
+            yield Checkbox(todo.text, 
+                           todo.done, 
+                           id=f'todo{i}')
+
+    def on_key(self, event: events.Event) -> None:
+        new_selected_index = self.selected_index
+        if event.key == 'j':
+            new_selected_index += 1
+        elif event.key == 'k':
+            new_selected_index -= 1
+
+        selected = self.query_one(f'#todo{new_selected_index}')
+
+        if selected:
+            self.selected_index = new_selected_index
+            selected.focus()
+
+        with open(self.filename, 'w') as f:
+            json.dump([asdict(todo) for todo in self.todos], f, indent=4)
 
 if __name__ == '__main__':
     app = MyApp()
