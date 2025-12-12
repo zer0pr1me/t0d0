b854c71d2c2b441b497ddcc19488977b884065e6
0 <zeroprime@mailbox.org>
2025-12-12 04:48:05 +0200

    Fix reading JSON

--- todolist/json_todolist.py
+++ todolist/json_todolist.py
@@ -9,17 +9,17 @@ class JsonTodoList(TodoList):
     def __init__(self, name: str, dirname: str):
         super().__init__(name)
         self.dir = Path(dirname)
-        self.todo_file = Path(dirname / 'todo-list.json')
+        self.todo_file = self.dir / 'todo-list.json'
 
     def save(self):
         self.dir.mkdir(parents=True, exist_ok=True)
-        with open(self.dir / 'todo-list.json', 'w') as f:
+        with open(self.todo_file, 'w') as f:
             json.dump([asdict(todo) for todo in self.todos], f, indent=4)
 
     def load(self):
         if not self.todo_file.exists():
             self.todos = []
             return
-        with open(self.dir / 'todo-list.json', 'r') as f:
+        with open(self.todo_file, 'r') as f:
             self.todos = [Todo(**todo) 
                             for todo in json.load(f)]
